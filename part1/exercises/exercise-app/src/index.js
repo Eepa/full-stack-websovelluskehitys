e40fcf44e58af9135dbb387dfc3da320b585e184
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-24 21:33:27 +0200

    Exercise 1.4 done

--- part1/exercises/exercise-app/src/index.js
+++ part1/exercises/exercise-app/src/index.js
@@ -4,19 +4,20 @@ import ReactDOM from 'react-dom'
 const App = () => {
 
     const kurssi = 'Half Stack -sovelluskehitys';
-    const osa1 = {
-        nimi: 'Reactin perusteet',
-        tehtavia: 10
-    };
-
-    const osa2 = {
-        nimi: 'Tiedonvälitys propseilla',
-        tehtavia: 7
-    };
-    const osa3 = {
-        nimi: 'Komponenttien tila',
-        tehtavia: 14
-    };
+    const osat = [
+        {
+            nimi: 'Reactin perusteet',
+            tehtavia: 10
+        },
+        {
+            nimi: 'Tiedonvälitys propseilla',
+            tehtavia: 7
+        },
+        {
+            nimi: 'Komponenttien tila',
+            tehtavia: 14
+        }
+    ];
 
     const Otsikko = (props) => {
         return (
@@ -33,24 +34,24 @@ const App = () => {
     const Sisalto = (props) => {
         return (
             <div>
-                <Osa coursePart={props.coursePart1} />
-                <Osa coursePart={props.coursePart2} />
-                <Osa coursePart={props.coursePart3} />
+                <Osa coursePart={props.courseParts[0]} />
+                <Osa coursePart={props.courseParts[1]} />
+                <Osa coursePart={props.courseParts[2]} />
             </div>
         )
     };
 
     const Yhteensa = (props) => {
         return (
-            <p>yhteensä {props.totalExerciseAmount} tehtävää</p>
+            <p>yhteensä {props.courseParts[0].tehtavia + props.courseParts[1].tehtavia + props.courseParts[2].tehtavia} tehtävää</p>
         )
     };
 
     return (
         <div>
             <Otsikko course={kurssi}/>
-            <Sisalto coursePart1={osa1} coursePart2={osa2} coursePart3={osa3}/>
-            <Yhteensa totalExerciseAmount={osa1.tehtavia+osa2.tehtavia+osa3.tehtavia}/>
+            <Sisalto courseParts={osat}/>
+            <Yhteensa courseParts={osat}/>
         </div>
     )
 };
