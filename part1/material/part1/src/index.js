1ba08c71c13907f2039c0f7af55e5806353cb1e5
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-26 11:19:00 +0200

    Start view rendering stuff

--- part1/material/part1/src/index.js
+++ part1/material/part1/src/index.js
@@ -1,43 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-class Hello extends React.Component {
-
-
-
-    render() {
-        const {name, age} = this.props;
-        const bornYear = () => 1900 + new Date().getYear() - age;
-
-
-        return (
-            <div>
-                <p>
-                    Hello {name}, you are {age} years old <br />
-                    So you were propably born {bornYear()}
-                </p>
-            </div>
-        )
-    }
-}
-
-const Footer = () => {
-    return (
-        <div>Greeting app created by <a href="https://github.com/mluukkai">mluukkai</a></div>
-    )
+const App = (props) => {
+  const {counter} = props;
+  return (
+      <div>{counter.value}</div>
+  )
 };
 
-const App = () => {
-    const name = 'Pekka';
-    const age = 10;
-    return (
-        <div>
-            <h1>Greetings</h1>
-            <Hello name="Arto" age={26 + 10}/>
-            <Hello name={name} age={age}/>
-            <Footer/>
-        </div>
+const counter = {
+    value: 1
+};
+
+const renderView = () => {
+    ReactDOM.render(
+        <App counter={counter}/>, document.getElementById('root')
     );
 };
 
-ReactDOM.render(<App/>, document.getElementById('root'));
+setInterval(() => {
+    renderView();
+    counter.value += 1;
+}, 1000);
