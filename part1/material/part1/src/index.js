c0f90aada39b07b461567a7c687a1b4aaca2afa5
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-26 16:26:28 +0200

    Simple state

--- part1/material/part1/src/index.js
+++ part1/material/part1/src/index.js
@@ -1,25 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+const Display = ({counter}) => <div>{counter}</div>;
+
+const Button = ({handleClick, text}) => (
+  <button onClick={handleClick}>
+      {text}
+  </button>
+);
+
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             counter: 1
         };
-
-        setInterval(() => {
-            this.setState({
-                counter: this.state.counter + 1
-            });
-        }, 1000)
+        // this.addOne = this.addOne.bind(this);
+        // this.clear = this.clear.bind(this);
     }
 
-    render() {
-        console.log("Rendering!", this.state.counter);
-        return (
+    setValue = (newValue) => () => { this.setState({counter: newValue})};
 
-            <div>{this.state.counter}</div>
+
+    addOne = () =>  {
+        console.log(this);
+        this.setState(
+            {counter: this.state.counter + 1}
+        )
+    };
+
+    clear = () => {
+        this.setState(
+            {counter: 0}
+        )
+    };
+
+    render() {
+        return (
+            <div>
+                <Display counter={this.state.counter}/>
+                <div>
+
+                    <Button
+                        handleClick={this.setValue(this.state.counter + 1)}
+                        text={"Plus"}
+                    />
+
+                    <Button
+                        handleClick={this.setValue(this.state.counter - 1)}
+                        text={"Minus"}
+                    />
+
+                    <Button
+                        handleClick={this.setValue(0)}
+                        text={"Zero"}
+                    />
+
+                </div>
+            </div>
         )
     }
 }
