827d168ebf1d13d9b3e400ae1376d0272fc2c8d8
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-28 15:36:01 +0200

    Adding new note

--- part2/material/part2/src/App.js
+++ part2/material/part2/src/App.js
@@ -1,15 +1,57 @@
 import React from 'react'
 import Note from './components/Note'
 
-const App = ({notes}) => {
-    return (
-        <div>
-            <h1>Muistiinpanot</h1>
-            <ul>
-                {notes.map(note => <Note key={note.id} note={note}/>)}
-            </ul>
-        </div>
-    )
-};
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            notes: props.notes,
+            newNote: "Uusi muistiinpano..."
+        }
+    }
+
+    addNote = (event) => {
+        event.preventDefault();
+        const newNote = {
+            content: this.state.newNote,
+            date: new Date().new,
+            important: Math.random() > 0.5,
+            id: this.state.notes.length + 1
+        };
+
+        const notes = this.state.notes.concat(newNote);
+
+        this.setState({
+            notes: notes,
+            newNote: ''
+        })
+    };
+
+    handleNoteChange = (event) => {
+        console.log(event.target.value);
+        this.setState({
+            newNote: event.target.value
+        })
+    };
+
+    render() {
+        return (
+            <div>
+                <h1>Muistiinpanot</h1>
+                <ul>
+                    {this.state.notes.map(note => <Note key={note.id} note={note} />)}
+                </ul>
+
+                <form onSubmit={this.addNote}>
+                    <input
+                        value={this.state.newNote}
+                        onChange={this.handleNoteChange}
+                    />
+                    <button type="submit">Tallenna</button>
+                </form>
+            </div>
+        )
+    }
+}
 
 export default App;
