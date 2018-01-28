57227bdd4526e87d1a882cdbc16118f443a53aa8
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-28 15:07:59 +0200

    Exercise 2.5. done

--- part2/exercises/exercise-app/src/index.js
+++ part2/exercises/exercise-app/src/index.js
@@ -1,96 +1,54 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-const Otsikko = ({courseName}) => {
-    return (
-        <h1>{courseName}</h1>
-    )
-};
-
-const Osa = ({coursePart}) => {
-    return (
-        <p>{coursePart.nimi} {coursePart.tehtavia}</p>
-    )
-};
-
-const Sisalto = ({courseParts}) => {
-    return (
-        <div>
-            {courseParts.map(coursePart => <Osa key={coursePart.id} coursePart={coursePart}/>)}
-        </div>
-    )
-};
-
-const Yhteensa = ({courseParts}) => {
-
-    const calculateTaskSum = (taskSum, part) => {
-        return taskSum + part.tehtavia;
-    };
-
-    let taskSum = courseParts.reduce(calculateTaskSum, 0);
-
-    return (
-        <p>yhteensä {taskSum} tehtävää</p>
-    )
-};
-
-const Kurssi = ({course}) => {
-    return (
-        <div>
-            <Otsikko courseName={course.nimi}/>
-            <Sisalto courseParts={course.osat}/>
-            <Yhteensa courseParts={course.osat}/>
-        </div>
-    )
-};
+import Course from './components/Course'
 
 const App = () => {
 
-    const kurssit = [
+    const courses = [
         {
             id: 1,
-            nimi: 'Half Stack -sovelluskehitys',
-            osat: [
+            name: 'Half Stack -sovelluskehitys',
+            parts: [
                 {
                     id: 1,
-                    nimi: 'Reactin perusteet',
-                    tehtavia: 10,
+                    name: 'Reactin perusteet',
+                    tasks: 10,
                 },
                 {
                     id: 2,
-                    nimi: 'Tiedonvälitys propseilla',
-                    tehtavia: 7
+                    name: 'Tiedonvälitys propseilla',
+                    tasks: 7
                 },
                 {
                     id: 3,
-                    nimi: 'Komponenttien tila',
-                    tehtavia: 14
+                    name: 'Komponenttien tila',
+                    tasks: 14
                 },
                 {
                     id: 4,
-                    nimi: 'Testiosa',
-                    tehtavia: 10
+                    name: 'Testiosa',
+                    tasks: 10
                 },
                 {
                     id: 5,
-                    nimi: 'Testiosa 2',
-                    tehtavia: 0
+                    name: 'Testiosa 2',
+                    tasks: 0
                 }
             ]
         },
         {
             id: 2,
-            nimi: 'Node.js',
-            osat: [
+            name: 'Node.js',
+            parts: [
                 {
                     id: 4,
-                    nimi: 'Testiosa',
-                    tehtavia: 10
+                    name: 'Testiosa',
+                    tasks: 10
                 },
                 {
                     id: 5,
-                    nimi: 'Testiosa 2',
-                    tehtavia: 0
+                    name: 'Testiosa 2',
+                    tasks: 0
                 }
             ]
         }
@@ -98,7 +56,7 @@ const App = () => {
 
     return (
         <div>
-            {kurssit.map(course => <Kurssi key={course.id} course={course}/>)}
+            {courses.map(course => <Course key={course.id} course={course}/>)}
         </div>
     )
 };
