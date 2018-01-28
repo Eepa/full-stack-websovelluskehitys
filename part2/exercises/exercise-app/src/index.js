601fc5161708268f7802534f43b550a911665c9c
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-28 14:30:04 +0200

    Exercise 2.1 done

--- part2/exercises/exercise-app/src/index.js
+++ part2/exercises/exercise-app/src/index.js
@@ -1,60 +1,77 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+const Otsikko = ({courseName}) => {
+    return (
+        <h1>{courseName}</h1>
+    )
+};
+
+const Osa = ({coursePart}) => {
+    return (
+        <p>{coursePart.nimi} {coursePart.tehtavia}</p>
+    )
+};
+
+const Sisalto = ({courseParts}) => {
+    return (
+        <div>
+            {courseParts.map(coursePart => <Osa key={coursePart.id} coursePart={coursePart}/>)}
+        </div>
+    )
+};
+
+/*const Yhteensa = (props) => {
+    return (
+        <p>yhteensä {props.courseParts[0].tehtavia + props.courseParts[1].tehtavia + props.courseParts[2].tehtavia} tehtävää</p>
+    )
+};*/
+
+const Kurssi = ({course}) => {
+    return (
+        <div>
+            <Otsikko courseName={course.nimi}/>
+            <Sisalto courseParts={course.osat}/>
+            {/*<Yhteensa courseParts={kurssi.osat}/>*/}
+        </div>
+    )
+};
+
 const App = () => {
 
     const kurssi = {
         nimi: 'Half Stack -sovelluskehitys',
         osat: [
             {
+                id: 1,
                 nimi: 'Reactin perusteet',
-                tehtavia: 10
+                tehtavia: 10,
             },
             {
+                id: 2,
                 nimi: 'Tiedonvälitys propseilla',
                 tehtavia: 7
             },
             {
+                id: 3,
                 nimi: 'Komponenttien tila',
                 tehtavia: 14
+            },
+            {
+                id: 4,
+                nimi: 'Testiosa',
+                tehtavia: 10
+            },
+            {
+                id: 5,
+                nimi: 'Testiosa 2',
+                tehtavia: 2
             }
         ]
     };
 
-    const Otsikko = (props) => {
-        return (
-            <h1>{props.courseName}</h1>
-        )
-    };
-
-    const Osa = (props) => {
-        return (
-            <p>{props.coursePart.nimi} {props.coursePart.tehtavia}</p>
-        )
-    };
-
-    const Sisalto = (props) => {
-        return (
-            <div>
-                <Osa coursePart={props.courseParts[0]} />
-                <Osa coursePart={props.courseParts[1]} />
-                <Osa coursePart={props.courseParts[2]} />
-            </div>
-        )
-    };
-
-    const Yhteensa = (props) => {
-        return (
-            <p>yhteensä {props.courseParts[0].tehtavia + props.courseParts[1].tehtavia + props.courseParts[2].tehtavia} tehtävää</p>
-        )
-    };
-
     return (
-        <div>
-            <Otsikko courseName={kurssi.nimi}/>
-            <Sisalto courseParts={kurssi.osat}/>
-            <Yhteensa courseParts={kurssi.osat}/>
-        </div>
+        <Kurssi course={kurssi}/>
     )
 };
 
