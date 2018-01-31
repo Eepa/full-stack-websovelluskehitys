749f7df291da73685008af827bb7e421432004e6
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-31 15:24:49 +0200

    Start adding axios

--- part2/material/part2/src/index.js
+++ part2/material/part2/src/index.js
@@ -1,6 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './App'
+import axios from 'axios'
+
+axios
+    .get('http://localhost:3001/notes')
+    .then(response => {
+        const notes = response.data;
+        console.log(notes);
+    });
+
+/*const promise2 = axios.get('http://localhost:3001/foobar');
+console.log(promise2);*/
 
 const notes = [
     {
