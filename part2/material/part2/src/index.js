991290b3192d09103f076b197e883b47b946c0f7
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-01-31 15:39:54 +0200

    Bad way to fetch data

--- part2/material/part2/src/index.js
+++ part2/material/part2/src/index.js
@@ -7,34 +7,9 @@ axios
     .get('http://localhost:3001/notes')
     .then(response => {
         const notes = response.data;
-        console.log(notes);
+        ReactDOM.render(
+            <App notes={notes} />,
+            document.getElementById('root')
+        );
     });
 
-/*const promise2 = axios.get('http://localhost:3001/foobar');
-console.log(promise2);*/
-
-const notes = [
-    {
-        id: 1,
-        content: 'HTML on helppoa',
-        date: '2017-12-10T17:30:31.098Z',
-        important: true
-    },
-    {
-        id: 2,
-        content: 'Selain pystyy suorittamaan vain javascriptiä',
-        date: '2017-12-10T18:39:34.091Z',
-        important: false
-    },
-    {
-        id: 3,
-        content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
-        date: '2017-12-10T19:20:14.298Z',
-        important: true
-    }
-];
-
-ReactDOM.render(
-    <App notes={notes} />,
-    document.getElementById('root')
-);
