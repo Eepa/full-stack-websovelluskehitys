750e6da88312c77718b098b67257675578ce0e2e
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-02-04 12:23:08 +0200

    Exercise 2.13 done

--- part2/exercises/country-information/src/App.js
+++ part2/exercises/country-information/src/App.js
@@ -1,47 +1,6 @@
 import React from 'react'
 import countryService from './services/countries'
-
-const Country = ({country}) => {
-    return (
-        <div>
-            <h2>{country.name}</h2>
-            <p>
-                Capital: {country.capital}
-            </p>
-            <p>
-                Population: {country.population}
-            </p>
-            <img src={country.flag} alt={`The flag of ${country.name}`}/>
-        </div>
-    )
-};
-
-const Countries = ({countries, filter}) => {
-    const filterCountry = (country) => {
-        const filterValue = filter.toLowerCase();
-        const lowerCaseName = country.name.toLowerCase();
-
-        return lowerCaseName.indexOf(filterValue) !== -1;
-    };
-
-    const countriesToShow = countries.filter(filterCountry);
-
-    if(countriesToShow.length > 10) {
-        return (
-            <div>Too many matches. Specify another filter.</div>
-        )
-    } else if (countriesToShow.length === 1) {
-        return (
-            <Country country={countriesToShow[0]}/>
-        )
-    }
-
-    return (
-        <div>
-            {countriesToShow.map(country => <div key={country.numericCode}>{country.name}</div>)}
-        </div>
-    )
-};
+import Countries from './components/Countries'
 
 class App extends React.Component {
 
@@ -70,7 +29,14 @@ class App extends React.Component {
         })
     };
 
-
+    selectOneCountry = (countryName) => {
+        return () => {
+            this.setState({
+                filter: countryName
+            })
+        }
+    };
+    
     render() {
 
         return (
@@ -82,7 +48,7 @@ class App extends React.Component {
                         onChange={this.handleFilterChange}
                     />
                 </div>
-                <Countries countries={this.state.countries} filter={this.state.filter} />
+                <Countries countries={this.state.countries} filter={this.state.filter} selectCountry={this.selectOneCountry} />
             </div>
         )
     }
