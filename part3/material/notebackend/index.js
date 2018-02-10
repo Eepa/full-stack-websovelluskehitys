3b24ab4c484482cc094d627501cf92ea908f4091
Eveliina Pakarinen <eveliina.pakarinen@cinia.fi>
2018-02-11 01:54:27 +0200

    Material until express

--- part3/material/notebackend/index.js
+++ part3/material/notebackend/index.js
@@ -1,1 +1,37 @@
-console.log("Hello world");
+const express = require('express');
+const app = express();
+
+let notes = [
+    {
+        id: 1,
+        content: 'HTML on helppoa',
+        date: '2017-12-10T17:30:31.098Z',
+        important: true
+    },
+    {
+        id: 2,
+        content: 'Selain pystyy suorittamaan vain javascriptiä',
+        date: '2017-12-10T18:39:34.091Z',
+        important: false
+    },
+    {
+        id: 3,
+        content: 'HTTP-protokollan tärkeimmät metodit ovat GET ja POST',
+        date: '2017-12-10T19:20:14.298Z',
+        important: true
+    }
+];
+
+app.get('/', (request, response) => {
+    response.send('<h1>Hello world!</h1>');
+});
+
+app.get('/notes', (request, response) =>  {
+    response.json(notes);
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
